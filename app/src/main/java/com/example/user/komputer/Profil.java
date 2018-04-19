f780f42782d6732f9d4dd636b6fb7a1b34445a81
IrhamMaulani <irhammaulani@gmail.com>
2018-04-19 22:58:50 +0800

    penambahan profil,tombol back untuk activity ke fragment,halaman profil,dan penambahan bug

--- app/src/main/java/com/example/user/komputer/Profil.java
+++ app/src/main/java/com/example/user/komputer/Profil.java
@@ -1,13 +1,60 @@
 package com.example.user.komputer;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Profil extends AppCompatActivity {
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent parentIntent = NavUtils.getParentActivityIntent(this);
+                if(parentIntent == null) {
+                    finish();
+                    return true;
+                } else {
+                    parentIntent.setFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    startActivity(parentIntent);
+                    finish();
+                    return true;
+                }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        Toolbar ToolBarAtas2 = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(ToolBarAtas2);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+       EditText et1 = (EditText) findViewById(R.id.edit_text_nama);
+       et1.setText("Irham");
+
+
+    }
+
+
+
+
+    public void submit(View view) {
+        view.startAnimation(buttonClick);
+        Toast.makeText(this, "Data Anda Telah Tersimpan", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/user/komputer/ProfilFragment.java
+++ app/src/main/java/com/example/user/komputer/ProfilFragment.java
@@ -2,20 +2,23 @@ package com.example.user.komputer;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfilFragment extends Fragment implements  View.OnClickListener {
-
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
 
     public ProfilFragment() {
         // Required empty public constructor
@@ -23,6 +26,7 @@ public class ProfilFragment extends Fragment implements  View.OnClickListener {
 
 
     ImageView goToEdit;
+    LinearLayout goToHelp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,12 +37,33 @@ public class ProfilFragment extends Fragment implements  View.OnClickListener {
         goToEdit = (ImageView) myView.findViewById(R.id.edit_profil);
         goToEdit.setOnClickListener(this);
 
+        goToHelp = (LinearLayout) myView.findViewById(R.id.goToHelp);
+        goToHelp.setOnClickListener(this);
+
         return myView;
     }
 
     public void onClick(View v) {
-        Intent intent = new Intent(getActivity(),Profil.class);
-        getActivity().startActivity(intent);
+        v.startAnimation(buttonClick);
+
+        switch(v.getId())
+        {
+            case R.id.edit_profil:
+                Intent intent = new Intent(getActivity(),Profil.class);
+                getActivity().startActivity(intent);
+
+                break;
+
+            case  R.id.goToHelp:
+                String url = "https://github.com/IrhamMaulani/Aplikasi-Komputer-Online";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                break;
+
+
+        }
+
     }
 
 }
